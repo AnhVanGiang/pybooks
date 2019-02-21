9d3ee4c49631039b2daaef9ee6e6a94cb9d6d339
AnhVanGiang <vangianganh@gmail.com>
2019-02-22 00:11:50 +0100

    Add files via upload
    Add sdebugger

--- setup.py
+++ setup.py
@@ -3,19 +3,21 @@ from setuptools import setup
 setup(
     name='pybooks',
     packages=['pybooks'],
-    version='0.4',
+    version='1.1',
     license='MIT',
     description='Find a book download URL from multiple online book sources',
     author='Anh Van Giang',
     author_email='vangianganh@gmail.com',
     url='https://github.com/AnhVanGiang/pybooks',
-    download_url='https://github.com/AnhVanGiang/pybooks/archive/v_04.tar.gz',
-    package_data = {'pybooks': ['sources.json']},
+    download_url='https://github.com/AnhVanGiang/pybooks/archive/v_11.tar.gz',
+    package_data={'pybooks': ['sources.json']},
     include_package_data=True,
     keywords=['scraper', 'books', 'python'],
     install_requires=[
         'requests',
         'bs4',
+        'lxml',
+        'sdebugger'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
